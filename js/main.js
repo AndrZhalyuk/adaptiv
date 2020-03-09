cee093465f4fe80f23bbb55e078731abefc19476
AndrZhalyuk <andr03409@gmail.com>
2020-03-09 18:25:12 +0200

    added humburger

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+var burger = document.querySelector('.humburger');
+var menu = document.querySelector('.menu');
 
+burger.addEventListener('click', function () {
+
+  if (menu.classList.contains('menu-hide')) {
+    menu.classList.remove('menu-hide');
+    menu.classList.add('menu-open');
+  } else {
+    menu.classList.add('menu-hide');
+    menu.classList.remove('menu-open');
+  }
+});
